9aae4e8918f4649c6d86c9f0431305d2acb35f85
LiudmylaSK <liudmyla.s.k.18@gmail.com>
2023-08-25 20:15:26 +0300

    updates Task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,39 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const listEl = document.querySelector('.gallery');
+
+const renderList = (arr, container) => {
+  const markup = arr
+    .map(
+      ({ original, preview, description }) =>
+        `<li class="gallery__item" style="list-style: none">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>`
+    )
+    .join('');
+  container.insertAdjacentHTML('beforeend', markup);
+};
+
+renderList(galleryItems, listEl);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
+
 console.log(galleryItems);
