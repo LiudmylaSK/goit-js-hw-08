45e576c8fa5aaf69f285ab39630871154eea1ea7
LiudmylaSK <liudmyla.s.k.18@gmail.com>
2023-08-25 21:40:29 +0300

    updates Task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const updateLocalStorageThrottled = throttle(currentTime => {
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000);
+
+player.on('timeupdate', function (data) {
+  const currentTime = data.seconds;
+  updateLocalStorageThrottled(currentTime);
+});
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime !== null) {
+  player
+    .setCurrentTime(parseFloat(savedTime))
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+
+        default:
+          break;
+      }
+    });
+}
